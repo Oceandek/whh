2c03f070dcfd10bdaa6784e7df9e02de62675cd7
Ocean <156903851+Oceandek@users.noreply.github.com>
2024-09-16 20:37:15 +0200

    Update ok.lua

--- ok.lua
+++ ok.lua
@@ -3,14 +3,35 @@ local HttpService = game:GetService("HttpService")
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local player = game.Players.LocalPlayer
 local username = player.Name
+local leaderstats = player.leaderstats
+
+
+
+local hugecount = 0
+
+
+local function abbreviateNumber(value)
+    local absValue = math.abs(value)
+    
+    if absValue >= 1e9 then
+        return string.format("%.1fB", value / 1e9)  -- Billion
+    elseif absValue >= 1e6 then
+        return string.format("%.1fM", value / 1e6)  -- Million
+    elseif absValue >= 1e3 then
+        return string.format("%.1fK", value / 1e3)  -- Thousand
+    else
+        return tostring(value)  -- Less than 1K, no notation needed
+    end
+end
 
 local function updateUser()
-    local diamondsObject = player.leaderstats:FindFirstChild("💎 Diamonds")
-    if diamondsObject then
-        local gemCount = diamondsObject.Value
-        print("Gem Count: " .. gemCount)
-
-
+    
+    local formattedGemCount = leaderstats.Diamonds.Value
+    local gemCount = abbreviateNumber(formattedGemCount)
+    for _,pet in pairs(saveMod.Get()["Inventory"]["Pet"]) do
+        if string.find(pet.id,"Huge") then
+            hugecount = hugecount + 1
+        end
     local url = "http://192.168.0.117:8080/update-user"
     local data = {
         Url = url,
@@ -18,7 +39,7 @@ local function updateUser()
         Headers = {
             ["Content-Type"] = "application/json"
         },
-        Body = HttpService:JSONEncode({ username = username, gems = gemCount, status = "online" })
+        Body = HttpService:JSONEncode({ username = username, gems = gemCount, status = "online", huges = hugecount })
     }
 
     local response = requests(data)
@@ -34,3 +55,4 @@ updateUser()
 while wait(60) do
     updateUser()
 end
+
